49d16ea8afc3eaca363ca8f533b10c6868f3f06d
John Mendonca <joaosinho@gmail.com>
2021-11-24 15:39:37 -0700

    Header bugfix and remove container config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,6 @@ module.exports = {
   mode: 'jit',
   purge: ['./pages/**/*.js', './components/**/*.js'],
   darkMode: 'class',
-  theme: {
-    container: {
-      center: true,
-      padding: {
-        DEFAULT: '4px',
-        md: '8px',
-      },
-    },
-  },
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
