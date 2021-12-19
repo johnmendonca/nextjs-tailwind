023a892244e49440989566146674e58db796093a
John Mendonca <joaosinho@gmail.com>
2021-12-18 18:19:24 -0700

    Update to tailwind 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.js', './components/**/*.js'],
+  content: ['./pages/**/*.js', './components/**/*.js'],
   darkMode: 'class',
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
-    require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/line-clamp'),
   ],
 }
