9f4a3180ca39b338fe87b3d5afb5a72a6392263b
John Mendonca <joaosinho@gmail.com>
2022-06-16 02:00:35 -0600

    Remove linting, optional tailwind plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,4 @@
 module.exports = {
   content: ['./pages/**/*.js', './components/**/*.js'],
-  darkMode: 'class',
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/line-clamp'),
-  ],
+  darkMode: 'class'
 }
